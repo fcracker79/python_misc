e8fb5a3c49b4931ae7475aa7aca03f5976eec674
fcracker79 <fcracker79@gmail.com>
2018-11-24 09:38:11 +0100

    Fixed search tree.

--- ai/trees.py
+++ ai/trees.py
@@ -1,5 +1,6 @@
 import collections
 import itertools
+import time
 import typing
 
 
@@ -20,94 +21,141 @@ class Graph:
     def get_children(self, node):
         return self.archs.get(node, [])
 
-g = Graph()
-g.add_arch('Arad', 'Zerind')
-g.add_arch('Arad', 'Timsoara')
-g.add_arch('Arad', 'Sibiu')
-g.add_arch('Zerind', 'Oradea')
-g.add_arch('Oradea', 'Sibiu')
-g.add_arch('Timsoara', 'Lugoj')
-g.add_arch('Lugoj', 'Mehadia')
-g.add_arch('Mehadia', 'Dobreta')
-g.add_arch('Dobreta', 'Craovia')
-g.add_arch('Craovia', 'Rimnicu Vilcea')
-g.add_arch('Craovia', 'Pitesti')
-g.add_arch('Rimnicu Vilcea', 'Sibiu')
-g.add_arch('Rimnicu Vilcea', 'Pitesti')
-g.add_arch('Sibiu', 'Faragas')
-g.add_arch('Faragas', 'Bucharest')
-g.add_arch('Pitesti', 'Bucharest')
-g.add_arch('Bucharest', 'Giurgiu')
-g.add_arch('Bucharest', 'Urziceni')
-g.add_arch('Urziceni', 'Hisrova')
-g.add_arch('Urziceni', 'Vaslui')
-g.add_arch('Hisrova', 'Eforie')
-g.add_arch('Vaslui', 'Iasi')
-g.add_arch('Iasi', 'Neamt')
+
+def _create_example_graph() -> Graph:
+    g = Graph()
+    g.add_arch('Arad', 'Zerind')
+    g.add_arch('Arad', 'Timsoara')
+    g.add_arch('Arad', 'Sibiu')
+    g.add_arch('Zerind', 'Oradea')
+    g.add_arch('Oradea', 'Sibiu')
+    g.add_arch('Timsoara', 'Lugoj')
+    g.add_arch('Lugoj', 'Mehadia')
+    g.add_arch('Mehadia', 'Dobreta')
+    g.add_arch('Dobreta', 'Craovia')
+    g.add_arch('Craovia', 'Rimnicu Vilcea')
+    g.add_arch('Craovia', 'Pitesti')
+    g.add_arch('Rimnicu Vilcea', 'Sibiu')
+    g.add_arch('Rimnicu Vilcea', 'Pitesti')
+    g.add_arch('Sibiu', 'Faragas')
+    g.add_arch('Faragas', 'Bucharest')
+    g.add_arch('Pitesti', 'Bucharest')
+    g.add_arch('Bucharest', 'Giurgiu')
+    g.add_arch('Bucharest', 'Urziceni')
+    g.add_arch('Urziceni', 'Hisrova')
+    g.add_arch('Urziceni', 'Vaslui')
+    g.add_arch('Hisrova', 'Eforie')
+    g.add_arch('Vaslui', 'Iasi')
+    g.add_arch('Iasi', 'Neamt')
+    return g
+
+
+Node = typing.NamedTuple(
+        'Node',
+        (
+            ('state', typing.Any),
+            ('parent', typing.Optional['Node']),
+            # ('operator', 'Boh'),
+            ('depth', int),
+            ('cost', int),
+        )
+    )
+Solution = typing.NamedTuple(
+    'Solution',
+    (
+        ('nodes', typing.List[Node]),
+        ('score', int)
+    )
+)
+
+QueuingFunction = typing.NewType(
+    'QueuingFunction',
+    typing.Callable[[typing.Deque[Node], typing.Sequence[Node]], typing.Deque[Node]]
+)
 
 
 class SearchTree:
     def __init__(
             self, graph: Graph, root: str, goal: str,
-            queuing_function: typing.Callable[[typing.List[str], typing.Sequence[str]], typing.List[str]]):
+            queuing_function: QueuingFunction):
         self.graph, self.root, self.goal = graph, root, goal
         self.queuing_function = queuing_function
         self.nodes = collections.deque()
-        self.parents = dict()  # type: typing.Dict[str, str]
-        self.operators = dict()  # type: typing.Dict[str, Boh]
-        self.depths = dict()  # type: typing.Dict[str, int]
-        self.costs = dict()  # type: typing.Dict[str, int]
-        self.nodes.append(root)
-        self.parents[root] = None
-        self.depths[root] = 0
-        self.costs[root] = 0
+        self.nodes.append(Node(root, None, 0, 0))
 
     def log(self, *a, **kw):
-        print(*a, **kw)
+        pass
+        # print(*a, **kw)
 
-    def solve(self):
+    def get_children(self, node: Node) -> typing.Iterable[Node]:
+        return map(
+            lambda d: Node(d, node, node.depth + 1, node.cost + 1),
+            self.graph.get_children(node.state)
+        )
+
+    def solve(self) -> typing.Optional[Solution]:
         best_solution, best_score = None, -1
         while self.nodes:
-            node = self.nodes.pop()
+            node = self.nodes.pop()  # type: Node
             self.log('Current path', node, list(self.get_parents(node))[::-1])
             self.log('Nodes', self.nodes)
-            if node == self.goal:
-                if self.costs[node] < best_score or best_score < 0:
-                    print(self.costs)
-                    best_solution, best_score = list(self.get_parents(node))[::-1], self.costs[node]
-                    print(best_solution)
+            if node.state == self.goal:
+                if node.cost < best_score or best_score < 0:
+                    self.log(node)
+                    best_solution, best_score = list(self.get_parents(node))[::-1], node.cost
+                    self.log(best_solution)
                 continue
-            children = self.graph.get_children(node)
-            parents = set(self.get_parents(node))
+            children = self.get_children(node)
+            parent_states = set(map(lambda d: d.state, self.get_parents(node)))
             self.log('children', node, children)
-            children = list(set(children) - parents - set(self.nodes))
+            children = list(filter(lambda d: d.state not in parent_states, children))
             self.log('children cleaned', children)
-            for c in children:
-                self.parents[c] = node
-                self.depths[c] = self.depths[node] + 1
-                self.costs[c] = self.costs[node] + 1
             self.nodes = self.queuing_function(self.nodes, children)
             node in self.nodes and self.nodes.remove(node)
 
-        print(self.costs)
-        return best_solution, best_score
+        return Solution(best_solution, best_score) if best_score > 0 else None
 
-    def get_parents(self, node: str):
-        while self.parents.get(node):
-            node = self.parents[node]
+    @classmethod
+    def get_parents(cls, node: Node):
+        while node.parent:
+            node = node.parent
             yield node
 
 
-def breadth_first(queue: typing.List[str], nodes: typing.Sequence[str]) -> typing.List[str]:
+def _breadth_first(queue: typing.Deque[Node], nodes: typing.Sequence[Node]) -> typing.Deque[Node]:
     queue.extend(nodes)
     return queue
 
 
-def depth_first(queue: typing.List[str], nodes: typing.Sequence[str]) -> typing.List[str]:
+def _depth_first(queue: typing.Deque[Node], nodes: typing.Sequence[Node]) -> typing.Deque[Node]:
     return collections.deque(
         itertools.chain(nodes, queue)
     )
 
 
-# print(SearchTree(g, 'Arad', 'Bucharest', breadth_first).solve())
-print(SearchTree(g, 'Arad', 'Bucharest', depth_first).solve())
+def _header(t: str):
+    print('********** {} **********'.format(t))
+
+
+def _print_solution(name: str, solution: typing.Optional[Solution]):
+    _header(name)
+    print('Score:', solution.score)
+    print('Best path:', solution.nodes)
+
+
+def _print_algo(name: str, graph: Graph, from_node: str, to_node: str, queuing_function: QueuingFunction):
+    start = time.time()
+    solution = SearchTree(graph, from_node, to_node, _breadth_first).solve()
+    stop = time.time()
+    _print_solution(name, solution)
+    print('Elapsed time', stop - start)
+
+
+def _run():
+    g = _create_example_graph()
+    _print_algo('BREADTH FIRST', g, 'Arad', 'Bucharest', _breadth_first)
+    _print_algo('DEPTH FIRST', g, 'Arad', 'Bucharest', _depth_first)
+
+
+if __name__ == '__main__':
+    _run()
